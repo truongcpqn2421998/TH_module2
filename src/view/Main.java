077921c196b281434930d2ded37d751b01432233
truongcpqn2421998 <truongcpqn2421998@gmail.com>
2021-10-25 10:53:19 +0700

    update

--- src/view/Main.java
+++ src/view/Main.java
@@ -22,7 +22,7 @@ public class Main {
             System.out.println("5. Tìm kiếm");
             System.out.println("6. Đọc từ file");
             System.out.println("7. Ghi vào file");
-            System.out.println("8. Thoát");
+            System.out.println("0. Thoát");
             System.out.println("Chọn chức năng");
             choice =scanner.nextInt();
             switch (choice){
